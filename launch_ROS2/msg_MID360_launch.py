5ae3b40caca2e968290cfee338f501a0f78a5c66
Tasman de Pury <tdepury@gmail.com>
2026-04-06 13:24:30 +0200

    update point cloud type + frame name

--- launch_ROS2/msg_MID360_launch.py
+++ launch_ROS2/msg_MID360_launch.py
@@ -5,12 +5,12 @@ from launch_ros.actions import Node
 import launch
 
 ################### user configure parameters for ros2 start ###################
-xfer_format   = 1    # 0-Pointcloud2(PointXYZRTL), 1-customized pointcloud format
+xfer_format   = 0    # 0-Pointcloud2(PointXYZRTL), 1-customized pointcloud format
 multi_topic   = 0    # 0-All LiDARs share the same topic, 1-One LiDAR one topic
 data_src      = 0    # 0-lidar, others-Invalid data src
 publish_freq  = 10.0 # freqency of publish, 5.0, 10.0, 20.0, 50.0, etc.
 output_type   = 0
-frame_id      = 'livox_frame'
+frame_id      = 'lidar_link'
 lvx_file_path = '/home/livox/livox_test.lvx'
 cmdline_bd_code = 'livox0000000001'
 
